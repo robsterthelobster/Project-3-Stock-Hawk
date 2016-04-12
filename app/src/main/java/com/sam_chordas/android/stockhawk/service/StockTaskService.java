57107cf4969a15ea80bf21d2614829db00970b93
Robin Chen <robin3chen@gmail.com>
2016-04-11 18:24:45 -0700

    auto indented 2 --> 4 spaces

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
@@ -33,8 +33,6 @@ public class StockTaskService extends GcmTaskService{
     private StringBuilder mStoredSymbols = new StringBuilder();
     private boolean isUpdate;
 
-    public StockTaskService(){}
-
     public StockTaskService(Context context){
         mContext = context;
     }
