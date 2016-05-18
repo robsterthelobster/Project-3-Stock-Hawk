eb7691970ba17829304a2909eadd6b601c067eca
Robin Chen <robin3chen@gmail.com>
2016-05-18 16:53:17 -0700

    basic widget
    lots of bugs appeared though (non widget-related)

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/CollectionWidgetProvider.java
@@ -0,0 +1,51 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.DetailActivity;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Created by robin on 5/16/2016.
+ */
+public class CollectionWidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_collection);
+
+            // Create an Intent to launch MainActivity
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            views.setRemoteAdapter(R.id.widget_list,
+                    new Intent(context, CollectionWidgetRemoteViewsService.class));
+
+            boolean useDetailActivity = context.getResources()
+                    .getBoolean(R.bool.widget_collection_enabled);
+            Intent clickIntentTemplate = useDetailActivity
+                    ? new Intent(context, DetailActivity.class)
+                    : new Intent(context, MyStocksActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_list, clickPendingIntentTemplate);
+            views.setEmptyView(R.id.widget_list, R.id.widget_empty);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/CollectionWidgetRemoteViewsService.java
@@ -0,0 +1,123 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+/**
+ * Created by robin on 5/16/2016.
+ */
+public class CollectionWidgetRemoteViewsService extends  RemoteViewsService{
+    public final String LOG_TAG = CollectionWidgetRemoteViewsService.class.getSimpleName();
+    private static final String[] STOCKS_COLUMNS = {
+            QuoteColumns._ID,
+            QuoteColumns.SYMBOL,
+            QuoteColumns.BIDPRICE,
+            QuoteColumns.PERCENT_CHANGE,
+            QuoteColumns.CHANGE,
+            QuoteColumns.ISUP
+    };
+    // these indices must match the projection
+    static final int INDEX_STOCKS_ID = 0;
+    static final int INDEX_STOCKS_SYMBOL = 1;
+    static final int INDEX_STOCKS_BIDPRICE = 2;
+    static final int INDEX_STOCKS_PERCENT_CHANGE = 3;
+    static final int INDEX_STOCKS_CHANGE = 4;
+    static final int INDEX_STOCKS_ISUP = 5;
+
+    @Override
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsService.RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                Uri uri = QuoteProvider.Quotes.CONTENT_URI;
+                data = getContentResolver().query(uri,
+                        STOCKS_COLUMNS,
+                        QuoteColumns.ISCURRENT + " = ?",
+                        new String[]{"1"},
+                        null);
+
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_list_item);
+                String symbol = data.getString(INDEX_STOCKS_SYMBOL);
+                String bid_price = data.getString(INDEX_STOCKS_BIDPRICE);
+                String change = data.getString(INDEX_STOCKS_PERCENT_CHANGE);
+                views.setTextViewText(R.id.stock_symbol, symbol);
+                views.setTextViewText(R.id.bid_price, bid_price);
+                views.setTextViewText(R.id.change, change);
+
+                int isUp = data.getInt(INDEX_STOCKS_ISUP);
+                if(isUp == 1){
+                    views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_green);
+                }else{
+                    views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+                }
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(INDEX_STOCKS_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
